f393b2928a72564b679e5eff079c0143de85d9f4
Julianeme <juliantve@gmail.com>
2021-08-26 18:31:23 -0500

    minor gramma corrections

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -26,8 +26,8 @@ def do_deploy(archive_path):
             (filename_ext, filename))
         run('rm /tmp/{}'.format(filename_ext))
         run('mv /data/web_static/releases/{}/web_static/* \
-            /data/web_static/releases/{}/'.format(filename))
-        run('rm -rf /data/web_static/releases/{}/web_static{}'
+            /data/web_static/releases/{}/'.format(filename, filename))
+        run('rm -rf /data/web_static/releases/{}/web_static'
             .format(filename))
         run('rm -rf /data/web_static/current')
         run('ln -s /data/web_static/releases/{} /data/web_static/current'
