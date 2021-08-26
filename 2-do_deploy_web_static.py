1b44fb63eac046d021fb6c40f312e5b3306c3655
Julianeme <juliantve@gmail.com>
2021-08-26 16:24:22 -0500

    uploading file for first time

---
+++ 2-do_deploy_web_static.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+"""
+script that generates a .tgz archive from the contents of the web_static
+folder of your AirBnB Clone repo, using the function do_pack
+"""
+
+from fabric.api import put, env, hosts, run
+import os
+
+env.hosts = ['34.73.56.44', '54.234.202.244']
+
+
+def do_deploy(archive_path):
+    # verificamos si el path existe
+    if os.path.exists(archive_path):
+        return(False)
+    try:
+        # Upload the archive to the / tmp / directory of the web server
+        put(archive_path, '/tmp/')
+        # get the filename in the following 2 lines
+        filename_ext = archive_path.split("/")[-1]
+        filename = filename_ext.split(".")[0]
+        # Uncompress the archive to folder /data/web_static/releases/<archive
+        run('tar zxvf /tmp/{} /data/web_static/releases/{}'.format
+            (filename_ext, filename))
+        # Delete the archive from the web server
+        run(f'rm -r /tmp/{}'.format(filename_ext))
+        # Delete the symbolic link /data/web_static/current from the web server
+        run('rm /data/web_static/current')
+        # Create a new the symbolic link /data/web_static/current on the
+        # web server, linked to the new version of your code
+        # (/data/web_static/releases/<archive filename without extension >
+        run('ln -s / data/web_static/releases/{} /data/web_static/current'
+            .format(filename))
+        return(True)
+    except:
+        return(False)
