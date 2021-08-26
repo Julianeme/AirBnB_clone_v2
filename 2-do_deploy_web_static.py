36a3a5abfb67ddb5e6060b6d9c83aa059ed5ae1c
Julianeme <juliantve@gmail.com>
2021-08-26 18:08:06 -0500

    minor gramma corrections

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -18,25 +18,18 @@ def do_deploy(archive_path):
     if os.path.exists(archive_path) is False:
         return(False)
     try:
-        # get the filename in the following 2 lines
         filename_ext = archive_path.split("/")[-1]
         filename = filename_ext.split(".")[0]
-        # Upload the archive to the / tmp / directory of the web server
         put(archive_path, '/tmp/')
         run('mkdir -p /data/web_static/releases/{}'.format(filename))
-        # Uncompress the archive to folder /data/web_static/releases/<archive
         run('tar -xzf /tmp/{} -C /data/web_static/releases/{}'.format
             (filename_ext, filename))
-        # Delete the archive from the web server
         run('rm /tmp/{}'.format(filename_ext))
         run('mv /data/web_static/releases/{}/web_static/* \
             /data/web_static/releases/{}/'.format(filename))
-        # Delete the symbolic link /data/web_static/current from the web server
-        run('rm -rf /data/web_static/releases/{}/web_static{}'
-            .format(filename))run('rm -rf /data/web_static/current')
-        # Create a new the symbolic link /data/web_static/current on the
-        # web server, linked to the new version of your code
-        # (/data/web_static/releases/<archive filename without extension >
+        run('rm -rf /data/web_static/releases/{}/web_static'
+            .format(filename))
+        run('rm -rf /data/web_static/current')
         run('ln -s /data/web_static/releases/{} /data/web_static/current'
             .format(filename))
         return(True)
