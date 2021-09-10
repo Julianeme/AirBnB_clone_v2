9aac502a15986684ad0964690a52d45b7517ead7
Julianeme <juliantve@gmail.com>
2021-09-10 17:29:25 -0500

    uploading file for first time

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application:
+    listens on 0.0.0.0, port 5000
+    Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    """
+    Returns HBNB
+    '"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardonw_appcontext(exc):
+        storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5000')
