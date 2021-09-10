9712e5d2b82e7ff44f105c4bf5b7e50162dd68a9
Julianeme <juliantve@gmail.com>
2021-09-10 13:35:54 -0500

    uploading files 6 and updating templates output

---
+++ web_flask/6-number_odd_or_even.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application:
+    listens on 0.0.0.0, port 5000
+    Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def index():
+    """
+    Hello HBNB page
+    '"""
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    Returns HBNB
+    '"""
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space)
+    '"""
+    text = text.replace("_", " ")
+    return 'C {}'.format(text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def p_text(text='is cool'):
+    """
+    display “Python ”, followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+    The default value of text is “is cool
+    '"""
+    text = text.replace("_", " ")
+    return 'Python {}'.format(text)
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    """
+    /number/<n>: display “n is a number” only if n is an integer
+    '"""
+    if type(n) is int:
+        return '{:d} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """
+    displays a HTML page only if n is an integer
+    """
+    if type(n) is int:
+        return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """
+    displays a HTML page only if n is an integer
+    """
+    if type(n) is int:
+        if n % 2 == 0:
+            kind = 'even'
+        else:
+            kind = 'odd'
+        return render_template('6-number_odd_or_even.html', n=n, kind=kind)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5000')
