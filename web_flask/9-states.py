4df44b514431e28745f0231f6e8840fb338dc9e8
Julianeme <juliantve@gmail.com>
2021-09-10 21:42:26 -0500

    templates/9-states.html

---
+++ web_flask/9-states.py
@@ -0,0 +1,49 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application:
+    listens on 0.0.0.0, port 5000
+    Routes:
+    /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+"""
+from models import storage
+from flask import Flask
+from flask import render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/states', strict_slashes=False)
+def states():
+    """
+    Returns list of all State objects present
+    in DBStorage sorted by name (A->Z)
+    '"""
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.route('/states/<int:id>', strict_slashes=False)
+def states_by_id():
+    """
+    Shows a state searched by id and all its cities
+    '"""
+    states = storage.all("State")
+    if type(id) is int and id in states:
+        name = states.name
+        id = 'State.' + state_id
+        states_dict = {}
+        for keys,values in states.items():
+            if states[keys] == id:
+                states_dict[id].append(states[values])
+
+    return render_template("9-states.html", name=name, states_dict=states_dict)
+
+
+@app.teardown_appcontext
+def teardonw_appcontext(exception):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port='5000')
