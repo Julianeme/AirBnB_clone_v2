60f69add89c6c5940e7fe4ea34e6276c562ad92c
Julianeme <juliantve@gmail.com>
2021-09-11 20:11:56 -0500

    cities dictionary added

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -22,7 +22,8 @@ def cities_list():
     '"""
     states = storage.all(State)
     cities = storage.all(City)
-    return render_template("8-cities_by_states.html", states=states, cities=cities)
+    return render_template("8-cities_by_states.html",
+                           states=states, cities=cities)
 
 
 @app.teardown_appcontext
